ff519fc85db7b3b22df8ecc43f39502dec70d407
“Anthony <anthonyjmcbride@icloud.com>
2017-08-03 14:53:39 -0400

    Added jquery animation to badges/language/frontend/backend div

--- public/portfolio.js
+++ public/portfolio.js
@@ -3,9 +3,39 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var windowScroll = $(this).scrollTop();
     console.log(windowScroll);
+  });
 
+  $(".jquery-badge").hide();
+  // $(".frontgradient-line").hide();
+  // $(".backgradient-line").hide();
+
+  $(".language-div").mouseenter(function() {
+    $(".frontgradient-line").fadeIn(600);
+  });
+
+  $(".language-div").mouseenter(function() {
+    $(".backgradient-line").fadeIn(600);
   });
 
 
+  $(".frontend-badge").hide().animate({
+    	opacity: '0'
+    });
+    $(".frontend-div").mouseenter(function() {
+        $(".frontend-badge").show().animate({
+        	opacity: '1',
+          marginTop: '-3px'
+        });
+    });
 
-});
+    $(".backend-badge").hide().animate({
+      	opacity: '0'
+      });
+      $(".backend-div").mouseenter(function() {
+          $(".backend-badge").show().animate({
+          	opacity: '1',
+            marginTop: '-3px'
+          });
+      });
+
+    });
