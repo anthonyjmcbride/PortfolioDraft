bfba9c449e963d2f271232f25d3f4bff06a4f268
“Anthony <anthonyjmcbride@icloud.com>
2017-08-07 18:01:25 -0400

    Added animate.css/Added effects via jquery/animate.css/detailed styling

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  // PARALLAX EFFECT
+
+
   function hideElement(element) {
     $(element).hide().animate({ opacity: '0'});
   };
-
   function hideElementWidth(element) {
     $(element).hide().animate({ opacity: '0', width: 0});
   };
 
-
+  $(".title-name").addClass('animated fadeInDown');
   hideElement(".frontend-badge");
   hideElement(".backend-badge");
   hideElement(".front-text");
@@ -18,9 +18,11 @@ $(document).ready(function() {
 
 
 
-
+  // FADE IN ANIMATION
   $(window).scroll(function() {
+    // Measures scroll from Top of the page
     var windowScroll = $(this).scrollTop();
+    // Function executing FadeIn
     function animateElement(element) {
         $(element).show().animate({
           opacity: '1',
@@ -28,6 +30,7 @@ $(document).ready(function() {
         },1500);
     };
     if (windowScroll > 380) {
+      // Elements targeted
       animateElement(".frontend-badge");
       animateElement(".backend-badge");
       animateElement(".front-text");
@@ -36,8 +39,12 @@ $(document).ready(function() {
     console.log(windowScroll);
   });
 
+
+// GRADIENT LINE ANIMATION
   $(window).scroll(function() {
+    // Measures scroll from Top of the page
     var windowScroll = $(this).scrollTop();
+    // Function revealing gradient line
     function animateElementWidth(element) {
         $(element).show().animate({
           opacity: '1',
@@ -45,11 +52,22 @@ $(document).ready(function() {
         },500);
     };
     if (windowScroll > 373) {
+      // Elements tageted
       animateElementWidth(".frontgradient-line");
       animateElementWidth(".backgradient-line");
         }
-    // console.log(windowScroll);
   });
+
+  // $(window).scroll(function() {
+  //   var windowScroll = $(this).scrollTop();
+  //   function animatedFadeIn(element) {
+  //     $(element).addClass('animated fadeIn');
+  //   };
+  //     if (windowScroll > 1037) {
+  //       animatedFadeIn("div#exTab3.container");
+  //     }
+  //   });
+
 
   // animateElement(".frontgradient-line");
   // animateElement(".backgradient-line");
